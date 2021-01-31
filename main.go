a287c4f92cad8b17d7e10b7a1afbf2e4c6c02fd5
ashraful005 <alam1809005@stud.kuet.ac.bd>
2021-01-31 21:07:38 +0600

    Variables and Datatypes in Go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+
+import "fmt"
+
+
+func main(){
+
+var name string = "MOSTAIN"
+
+var name2 , city string = "ASHRAFUL" , "CUMILLA"
+
+var c rune = 'A'
+
+var age int = 10
+
+var result float32 = 50.95
+
+fmt.Println(name, name2, city, c, age, result)
+
+}
