52e797b8ec03f01db3019f70c963256ef2142b9a
ashraful005 <alam1809005@stud.kuet.ac.bd>
2021-02-06 20:53:13 +0600

    primitive-datatypes

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name string
+
+//var c rune  
+
+//var age int 
+
+//var result float32 
+
+var isFound bool
+
+fmt.Println(isFound)
+
+fmt.Println(true && true)
+  
+fmt.Println(true && false)
+  
+fmt.Println(true || true)
+  
+fmt.Println(true || false)
+  
+fmt.Println(!true)
+
+}
