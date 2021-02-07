776c8539fcb916ee8c6c2bf8612b169529422654
ashraful005 <alam1809005@stud.kuet.ac.bd>
2021-02-07 22:44:11 +0600

    Maps done!

--- main.go
+++ main.go
@@ -1,34 +1,17 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main(){
 
-var students [3]string
+x := make(map[string]int)
 
-students[0] = "Asgor"
-students[1] = "Mainul"
-students[2] = "Anonnya"
+x["abir"] = 10
+x["aziz"] = 15
 
-x := students[0:2]
+//delete(x, "aziz")
 
 fmt.Println(x)
-
-var fruits []string
-
-fruits = append(fruits, "Apple", "Banana", "Mango")
-
-fmt.Println(fruits, len(fruits))
-
-fmt.Printf("%T\n", fruits)
-
-fmt.Printf("%T\n", students)
-
-a := reflect.TypeOf(students).Kind().String()
-b := reflect.TypeOf(fruits).Kind().String()
-
-fmt.Println(a)
-fmt.Println(b)
+fmt.Println(x["abir"])
 
 }
