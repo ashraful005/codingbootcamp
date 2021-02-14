8b980e11ac2ae726bf48d5120c8f2615390ef1a8
ashraful005 <alam1809005@stud.kuet.ac.bd>
2021-02-14 21:12:32 +0600

    switch-case completed

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ switch age{
        fmt.Print("Infant")
  case 3,4,5,6,7,8,9,10,11,12:
        fmt.Println("Children")
-fallthrough
+       fallthrough
  case 13,14,15,16,17,18,19:
        fmt.Println("Teenager")
        fallthrough
