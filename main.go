89eedd2c7fea2a8511984365b9944e801452a066
ashraful005 <alam1809005@stud.kuet.ac.bd>
2021-02-14 20:17:09 +0600

    if-else if-else

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ fmt.Scanf("%d", &age)
 
 if age < 3{
  fmt.Println("Infant")
+}else if age>2 && age<13{
+ fmt.Println("Children")
+}else if age>12 && age<=19{
+ fmt.Println("Teenager")
+}else{
+ fmt.Println("Adult")
 }
 
 fmt.Println(age)
